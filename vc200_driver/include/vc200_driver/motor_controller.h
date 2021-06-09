184832c53f560e20b8c0553e71669d60b0017e9f
Jakub Kąkiel <kubakakiel@gmail.com>
2021-06-09 21:04:18 +0200

    Average current as effort of motor controller

--- vc200_driver/include/vc200_driver/motor_controller.h
+++ vc200_driver/include/vc200_driver/motor_controller.h
@@ -9,42 +9,47 @@
 #include "pid/pid.h"
 #include "stalker_driver/STInterfaceClientUDP.h"
 #include "stalker_driver/motor_controller.h"
+#include "stalker_driver/current_measurement.h"
 
-namespace vc200_driver {
-struct joint_state {
-  joint_state() : position(0.0), velocity(0.0), effort(0.0) {}
-  double position;
-  double velocity;
-  double effort;
-};
-class MotorController {
- public:
-  MotorController(std::shared_ptr<STInterface::STInterfaceClientUDP> st_if, ros::NodeHandle &nh);
+namespace vc200_driver
+{
+  struct joint_state
+  {
+    joint_state() : position(0.0), velocity(0.0), effort(0.0) {}
+    double position;
+    double velocity;
+    double effort;
+  };
+  class MotorController
+  {
+  public:
+    MotorController(std::shared_ptr<STInterface::STInterfaceClientUDP> st_if, ros::NodeHandle &nh);
 
-  std::shared_ptr<Interface::DownstreamData::MovementOrderLeftRightFrame> motorDownstream;
-  std::shared_ptr<Interface::UpstreamData::MovementInformationLeftRightFrame> motorUpstream;
-  std::shared_ptr<Interface::UpstreamData::EncoderFrame> encUpstream;
-  std::shared_ptr<STInterface::STInterfaceClientUDP> stClient_;
-  ros::NodeHandle nh_;
-  ros::NodeHandle priv_nh_;
-  std::vector<hardware_interface::JointHandle> getJoints();
+    std::shared_ptr<Interface::DownstreamData::MovementOrderLeftRightFrame> motorDownstream;
+    std::shared_ptr<Interface::UpstreamData::MovementInformationLeftRightFrame> motorUpstream;
+    std::shared_ptr<Interface::UpstreamData::EncoderFrame> encUpstream;
+    std::shared_ptr<Interface::UpstreamData::CurrentMeasurementFrame> currentUpstream;
+    std::shared_ptr<STInterface::STInterfaceClientUDP> stClient_;
+    ros::NodeHandle nh_;
+    ros::NodeHandle priv_nh_;
+    std::vector<hardware_interface::JointHandle> getJoints();
 
-  void sendCommand(Interface::DownstreamData::MovementCommandDataset cmd);
+    void sendCommand(Interface::DownstreamData::MovementCommandDataset cmd);
 
-  void readData();
-  void writeData();
+    void readData();
+    void writeData();
 
- private:
-  PID leftChannelPid_, rightChannelPid_;
+  private:
+    PID leftChannelPid_, rightChannelPid_;
 
-  joint_state leftJointState;
-  joint_state rightJointState;
-  double leftVelocityCommand;
-  double rightVelocityCommand;
-  hardware_interface::JointHandle frontLeftJointHandle, rearLeftJointHandle, frontRightJointHandle,
-      rearRightJointHandle;
-  
-  double max_command;
-  int encoder_resolution;
-};
-}  // namespace vc200_driver
+    joint_state leftJointState;
+    joint_state rightJointState;
+    double leftVelocityCommand;
+    double rightVelocityCommand;
+    hardware_interface::JointHandle frontLeftJointHandle, rearLeftJointHandle, frontRightJointHandle,
+        rearRightJointHandle;
+
+    double max_command;
+    int encoder_resolution;
+  };
+} // namespace vc200_driver
